c23459e17ffde185c05a24463b4213b83e511d30
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-02-13 14:46:42 +0000

    Add Task4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 $CFILE -o cisfun
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
