991e43b7239476955950d84b86dcef5e893aa00f
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-02-13 15:00:17 +0000

    Add Task5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+* main - Entry point of the program
+*
+* This function prints a string with proper grammar, followed by a new line.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
