77394290f99e4440bb367c452bb3d9143b45b07c
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-02-13 15:06:10 +0000

    Add Task6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+/**
+* main - Prints the size of various types on the computer
+*
+* This function prints the size of various data types like char, int,
+* long int, long long int, and float in bytes.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-printf("Size of a char: %lu byte(s)\n", sizeof(char));
-printf("Size of an int: %lu byte(s)\n", sizeof(int));
-printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-printf("Size of a float: %lu byte(s)\n", sizeof(float));
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
