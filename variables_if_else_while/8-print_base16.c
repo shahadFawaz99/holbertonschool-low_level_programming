11ec79b09c9da064f1638b59ce03186f5339dfff
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-02-13 21:10:47 +0000

    Task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int num;
+for (num = 48; num <= 57; num++) /* طباعة الأرقام 0-9 باستخدام ASCII */
+putchar(num);
+for (num = 97; num <= 102; num++) /* طباعة الحروف a-f باستخدام ASCII */
+putchar(num);
+putchar('\n'); /* طباعة سطر جديد */
+return (0);
+}
