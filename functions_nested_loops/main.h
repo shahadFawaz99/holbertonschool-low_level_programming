e3abb6c27a16269eec3382c52035ee51f14aba75
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-02-16 12:51:41 +0000

    task3

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
-void print_alphabet_x10(void);  /* Add the prototype for the print_alphabet_x10 function */
+int _islower(int c);  /* Function prototype for _islower */
 #endif /* MAIN_H */
