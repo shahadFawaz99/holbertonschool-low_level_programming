33b6fc317b8050ffe7e6d2bc74be2a4ca2703feb
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-02-16 10:47:56 +0000

    task9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
-/**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
 int main(void)
 {
-int num1, num2;
-for (num1 = 0; num1 <= 8; num1++)  // First digit from 0 to 8
+int i;
+for (i = 0; i < 10; i++)
 {
-for (num2 = num1 + 1; num2 <= 9; num2++)  // Second digit from num1+1 to 9
+putchar(i + '0');
+if (i < 9)
 {
-putchar(num1 + '0');  // Print the first digit
-putchar(num2 + '0');  // Print the second digit
-if (num1 != 8 || num2 != 9)  // Check if it's the last combination
-{
-putchar(',');  // Print comma
-putchar(' ');  // Print space
+putchar(',');
+putchar(' ');
 }
 }
-}
-putchar('\n');  // Print a newline after all combinations
-return (0);
+putchar('\n');
+return 0;
 }
