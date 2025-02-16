4b6bc39765d2ab5f9d6b7d682a2c1fdc18e9970f
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-02-16 10:19:26 +0000

    task9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int num;
+for (num = 0; num <= 9; num++)
+{
+putchar(num + '0');  // Print the number as a character
+if (num != 9)  // Check if it's not the last number
+{
+putchar(',');  // Print a comma
+putchar(' ');  // Print a space
+}
+}
+putchar('\n');  // Print a new line at the end
+return (0);
+}
