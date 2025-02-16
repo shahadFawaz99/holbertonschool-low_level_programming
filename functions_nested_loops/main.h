030366afc719380ef4eaf6299caf15707a6658be
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-02-16 14:22:13 +0000

    task9

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -7,4 +7,5 @@ int print_sign(int n);
 int _abs(int n);
 int print_last_digit(int n); /* Function prototype for print_last_digit */
 void jack_bauer(void);
+void times_table(void); /* Function prototype for times_table */
 #endif /* MAIN_H */
