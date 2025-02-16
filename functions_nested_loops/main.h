63241154d6f16a32bd3144d1702af2f7c069f416
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-02-16 14:13:03 +0000

    task7

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,7 +2,8 @@
 #define MAIN_H
 int _putchar(char c);
 int _islower(int c);
-int _isalpha(int c);  /* Function prototype for _isalpha */
-int print_sign(int n); /* Function prototype for print_sign */
-int _abs(int n); /* Function prototype for _abs */
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int n); /* Function prototype for print_last_digit */
 #endif /* MAIN_H */
