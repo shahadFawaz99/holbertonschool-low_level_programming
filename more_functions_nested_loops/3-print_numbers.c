4010140b74828e6122c2cc83393701ed8cbb2ac3
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-02-28 17:37:24 +0000

    jk

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * print_numbers - Prints the numbers from 0 to 9, followed by a new line.
+ * print_numbers - prints the numbers from 0 to 9 followed by a new line
  */
 void print_numbers(void)
 {
-	int i;
+    char c;
 
-	for (i = 0; i <= 9; i++) /* Loop from 0 to 9 */
-	{
-		_putchar(i + '0'); /* Print the current number */
-	}
-	_putchar('\n'); /* Print a new line */
+    for (c = '0'; c <= '9'; c++)
+    {
+        _putchar(c);
+    }
+    _putchar('\n');
 }
--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Prints the numbers from 0 to 9, excluding 2 and 4.
+ * print_most_numbers - prints the numbers from 0 to 9, excluding 2 and 4
  */
 void print_most_numbers(void)
 {
-	char c;
+    char c;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-		if (c != '2' && c != '4') /* Skip printing 2 and 4 */
-		{
-			_putchar(c);
-		}
-	}
-	_putchar('\n'); /* Print a new line at the end */
+    for (c = '0'; c <= '9'; c++)
+    {
+        if (c != '2' && c != '4')
+        {
+            _putchar(c);
+        }
+    }
+    _putchar('\n');
 }
-
