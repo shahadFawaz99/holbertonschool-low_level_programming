152b66b9616b9b195c3c226b15e0d88ebdd7a442
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-02-28 17:29:35 +0000

    jk

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
+
 int _putchar(char c);  // تعريف الدالة _putchar
+
 void print_numbers(void);
+
 #endif /* MAIN_H */
