e326b030991b63b1aca8e8ef84a28464e3cfcc30
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-03-05 10:25:14 +0000

    k

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -22,4 +22,5 @@ char *string_toupper(char *str);
 char *cap_string(char *str);
 char *leet(char *str);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif /* MAIN_H */
