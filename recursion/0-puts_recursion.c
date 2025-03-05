4f2665e0f238c31f9051b7251c1ebc1cbbc345cd
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-03-05 11:53:25 +0000

    FF

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -5,11 +5,11 @@
 */
 void _puts_recursion(char *s)
 {
-if (*s == '\0')  // If we reached the end of the string (null terminator)
+if (*s == '\0')  /* If we reached the end of the string (null terminator) */
 {
-_putchar('\n');  // Print a new line
-return;  // Exit the function
+_putchar('\n');  /* Print a new line */
+return;  /* Exit the function */
 }
-_putchar(*s);  // Print the current character
-_puts_recursion(s + 1);  // Recursively call the function with the next character
+_putchar(*s);  /* Print the current character */
+_puts_recursion(s + 1);  /* Recursively call the function with the next character */
 }
