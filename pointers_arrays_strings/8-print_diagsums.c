45f55877e9c062078b12d961dcafd5ea2acd1b17
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-03-05 11:34:10 +0000

    k

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -11,14 +11,14 @@ void print_diagsums(int *a, int size)
 {
 int i;
 int sum1 = 0, sum2 = 0;
-// Loop over the rows of the matrix
+/* Loop over the rows of the matrix */
 for (i = 0; i < size; i++)
 {
-// sum1 is for the main diagonal
+/* sum1 is for the main diagonal */
 sum1 += *(a + i * size + i);
-// sum2 is for the secondary diagonal
+/* sum2 is for the secondary diagonal */
 sum2 += *(a + i * size + (size - i - 1));
 }
-// Print the results
+/* Print the results */
 printf("%d, %d\n", sum1, sum2);
 }
