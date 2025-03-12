3f113c6d0c3f45357a4d74510d521f00c5856e21
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-03-12 20:29:40 +0000

    L

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -2,12 +2,12 @@
 #include "dog.h"
 /**
 * free_dog - Frees the memory allocated for a dog
-* @d: Pointer to the dog to free
+* @d: Pointer to the dog structure to free
 */
 void free_dog(dog_t *d)
 {
 if (d != NULL)
-}
+{
 free(d->name);
 free(d->owner);
 free(d);
