85846b31d8aa019d188bfc4430c1adadad46ba55
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-03-12 08:04:18 +0000

    J

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,22 +12,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    unsigned int total_size;
-    void *ptr;
-    unsigned int i;  /* Define i before the loop */
-
-    if (nmemb == 0 || size == 0)
-        return (NULL);
-
-    total_size = nmemb * size;
-    ptr = malloc(total_size);
-
-    if (ptr == NULL)
-        return (NULL);
-
-    /* Initialize memory to zero */
-    for (i = 0; i < total_size; i++)
-        ((char *)ptr)[i] = 0;
-
-    return (ptr);
+unsigned int total_size;
+void *ptr;
+unsigned int i;  /* Define i before the loop */
+if (nmemb == 0 || size == 0)
+return (NULL);
+total_size = nmemb * size;
+ptr = malloc(total_size);
+if (ptr == NULL)
+return (NULL);
+/* Initialize memory to zero */
+for (i = 0; i < total_size; i++)
+((char *)ptr)[i] = 0;
+return (ptr);
 }
