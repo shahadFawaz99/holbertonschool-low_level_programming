0d4a56b0c5c653d972fe56bfbb69916e34e1e00d
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-03-12 19:50:53 +0000

    l

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,17 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
- * struct dog - Structure representing a dog
- * @name: Pointer to the dog's name (string)
- * @age: Dog's age (floating point number)
- * @owner: Pointer to the owner's name (string)
- */
+* struct dog - Structure representing a dog
+* @name: Pointer to the dog's name (string)
+* @age: Dog's age (floating point number)
+* @owner: Pointer to the owner's name (string)
+*/
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+char *name;
+float age;
+char *owner;
 };
 /* Function prototype for init_dog */
 void init_dog(struct dog *d, char *name, float age, char *owner);
