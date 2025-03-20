b60b994665a72160f74f73e4db4fa7f4276c31bd
root <you@example.com>
2025-03-20 21:51:22 +0000

    h

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -7,32 +7,32 @@
  */
 void print_all(const char * const format, ...)
 {
-va_list args;
-unsigned int i = 0;
-char *str, *sep = "";
+	va_list args;
+	unsigned int i = 0;
+	char *str, *sep = "";
 
-va_start(args, format);
-while (format && format[i])
-{
-if (format[i] == 'c' || format[i] == 'i' ||
-format[i] == 'f' || format[i] == 's')
-{
-printf("%s", sep);
-if (format[i] == 'c')
-printf("%c", va_arg(args, int));
-else if (format[i] == 'i')
-printf("%d", va_arg(args, int));
-else if (format[i] == 'f')
-printf("%f", va_arg(args, double));
-else
-{
-str = va_arg(args, char *);
-printf("%s", str ? str : "(nil)");
-}
-sep = ", ";
-}
-i++;
-}
-va_end(args);
-printf("\n");
+	va_start(args, format);
+	while (format && format[i])
+	{
+		if (format[i] == 'c' || format[i] == 'i' ||
+		    format[i] == 'f' || format[i] == 's')
+		{
+			printf("%s", sep);
+			if (format[i] == 'c')
+				printf("%c", va_arg(args, int));
+			else if (format[i] == 'i')
+				printf("%d", va_arg(args, int));
+			else if (format[i] == 'f')
+				printf("%f", va_arg(args, double));
+			else
+			{
+				str = va_arg(args, char *);
+				printf("%s", str ? str : "(nil)");
+			}
+			sep = ", ";
+		}
+		i++;
+	}
+	va_end(args);
+	printf("\n");
 }
